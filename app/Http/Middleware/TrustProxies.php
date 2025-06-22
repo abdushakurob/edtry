dc6a58ac02c519185e69bfd5faaf7472e8773af5
Abdushakur Bashir <abdulshakur.m2304203@st.futminna.edu.ng>
2025-06-22 20:13:15 +0100

    Fix TrustProxies middleware proxy definition and correct environment check in AppServiceProvider

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,7 +2,7 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    protected $proxies = '*'; // or use [‘*’] for Laravel >= 10.6
+    protected $proxies = ‘*’
 
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
