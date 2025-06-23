5d5ab710ace5a965494f969107c58664848a9f33
Abdushakur Bashir <abdulshakur.m2304203@st.futminna.edu.ng>
2025-06-23 21:20:57 +0100

    Refactor logout button in SidebarContent component to remove loading state and simplify functionality

--- resources/js/Components/dashboard/SidebarContent.vue
+++ resources/js/Components/dashboard/SidebarContent.vue
@@ -33,20 +33,11 @@
         <p class="text-xs text-gray-500">{{ user?.role }}</p>
         </div>
       </div>
-      <AppButton variant="outline" class="w-full justify-center gap-2" @click="handleLogout" :disabled="isLoggingOut">
-        <template v-if="!isLoggingOut">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-          </svg>
-          Logout
-        </template>
-        <template v-else>
-          <svg class="animate-spin h-4 w-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
-          Logging out...
-        </template>
+      <AppButton variant="outline" class="w-full justify-center gap-2" @click="$emit('logout')">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+        </svg>
+        Logout
       </AppButton>
     </div>
   </div>
@@ -54,9 +45,7 @@
 
 <script setup>
 import AppButton from '../common/Button.vue'
-import { ref } from 'vue'
 
-const isLoggingOut = ref(false)
 const props = defineProps({
   links: {
     type: Array,
