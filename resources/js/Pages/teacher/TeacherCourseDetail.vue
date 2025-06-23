1dd0052b46b0740876891b257343a2cb8957c724
Abdushakur Bashir <abdulshakur.m2304203@st.futminna.edu.ng>
2025-06-23 10:23:25 +0100

    Reposition Add Lesson button in TeacherCourseDetail component

--- resources/js/Pages/teacher/TeacherCourseDetail.vue
+++ resources/js/Pages/teacher/TeacherCourseDetail.vue
@@ -180,15 +180,7 @@
                              
                           </div>
 
-                          <button 
-                              @click="addNewLesson"
-                              class="flex items-center justify-center bg-accent hover:bg-highlight text-white font-medium py-2 px-3 rounded-md text-sm transition-colors"
-                            >
-                              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                              </svg>
-                              Add Lesson
-                            </button>
+                          
                         </div>
                       </div>
                     </div>
@@ -209,6 +201,15 @@
                   </button>
                 </div>
               </div>
+              <button 
+              @click="addNewLesson"
+               class="flex items-center justify-center bg-accent hover:bg-highlight text-white font-medium py-2 px-3 rounded-md text-sm transition-colors"
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                </svg>
+                Add Lesson
+              </button>
             </div>
             
             <div v-else-if="activeTab === 'students'">
